3e489d9102ba20cdf1d1b55d6420b20d722e1453
tiagosan040 <tiagosan040@gmail.com>
2026-02-19 18:24:59 -0300

    chore: disable additional TypeScript ESLint unsafe rules

--- api/eslint.config.mjs
+++ api/eslint.config.mjs
@@ -32,7 +32,9 @@ export default tseslint.config(
       "prettier/prettier": ["error", { endOfLine: "auto" }],
       '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
-      '@typescript-eslint/no-unsafe-member-access': 'off'
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unsafe-function-type': 'off',
     },
   },
 );
