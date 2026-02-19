4ea4d3eaac435bcb005028487f1c5abbb2a27727
tiagosan040 <tiagosan040@gmail.com>
2026-02-19 18:27:16 -0300

    feat: feat: add error handling for HTTP status codes in BrasilApi adapter

--- api/src/modules/address/infrastructure/http/adapters/brasilApi/brasilApi.adapter.ts
+++ api/src/modules/address/infrastructure/http/adapters/brasilApi/brasilApi.adapter.ts
@@ -7,7 +7,12 @@ import {
   AddressData,
 } from '@/modules/address/domain/entities/address.entity';
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  ServiceUnavailableException,
+} from '@nestjs/common';
 import { FindAddressResponse } from './interfaces/find-address-response.interface';
 
 @Injectable()
@@ -17,23 +22,45 @@ export class BrasilApiAdapter implements AddressAdapter {
   constructor(private readonly httpService: HttpService) {}
 
   async findAddress(input: findAddressInput): Promise<AddressData> {
-    const { zipCode } = input;
+    try {
+      const { zipCode } = input;
 
-    const url = `${this.apiUrl}/${zipCode}`;
+      const url = `${this.apiUrl}/${zipCode}`;
 
-    const response =
-      await this.httpService.axiosRef.get<FindAddressResponse>(url);
+      const response =
+        await this.httpService.axiosRef.get<FindAddressResponse>(url);
 
-    const data = response.data;
+      const data = response.data;
 
-    const address = new Address({
-      zipCode: data.cep,
-      state: data.state,
-      city: data.city,
-      neighborhood: data.neighborhood,
-      street: data.street,
-    });
+      const address = new Address({
+        zipCode: data.cep,
+        state: data.state,
+        city: data.city,
+        neighborhood: data.neighborhood,
+        street: data.street,
+      });
 
-    return address.toJSON();
+      return address.toJSON();
+    } catch (error) {
+      if (error.response?.status === 400) {
+        throw new BadRequestException(
+          `Invalid zip code format: ${input.zipCode}`,
+        );
+      }
+
+      if (error.response?.status === 404) {
+        throw new NotFoundException(
+          `Address not found for the provided zip code: ${input.zipCode}`,
+        );
+      }
+
+      if (error.response?.status === 500) {
+        throw new ServiceUnavailableException(
+          `Failed to retrieve address data for the provided zip code: ${input.zipCode}`,
+        );
+      }
+
+      throw error;
+    }
   }
 }
